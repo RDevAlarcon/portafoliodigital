b5c5db338f6448fbb4c9969bbb72a721955bdb19
RDevAlarcon <alarconcastroricardo@gmail.com>
2024-08-28 21:57:27 -0400

    tercera version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
-function enviarFormulario(){
-    alert ("datos enviados correctamente");
+document.addEventListener('DOMContentLoaded', function() {
+    asignarEventos();
+});
+
+function asignarEventos(){
+    let elBotonForm = document.getElementById("btnForm");
+    elBotonForm.addEventListener('click', mostrarMensaje);
+}
+
+function mostrarMensaje(){
+    // Limpiar los campos
+    let elNombre = document.getElementById("nombreContacto");
+    elNombre.value = "";
+    let elEmail = document.getElementById("emailContacto");
+    elEmail.value = "";
+    let elMensaje = document.getElementById("mensajeContacto");
+    elMensaje.value = "";
+
+    alert('Sus datos fueron registrados');
 }
